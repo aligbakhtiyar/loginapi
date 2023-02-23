e01bf212f6c8b3c3ca12ff88e487ebeda768db46
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-02-24 00:59:02 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import './App.css';
+import axios from "axios";
+
+function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  console.log({ email, password })
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleApi = () => {
+    console.log({ email, password })
+    axios.post('https://dev-mapi.zapsrewards.com/api/InternalLogin/login', {
+      email: email,
+      password: password,
+    }).then(result => {
+      console.log(result.data)
+      alert('success')
+    })
+      .catch(error => {
+        alert('service error')
+        console.log(error)
+      })
+  }
+
+  return (
+    <div className="App">
+      Email : <input value={email} onChange={handleEmail} type="text" /> <br />
+      Password : <input value={password} onChange={handlePassword} type="text" /> <br />
+      <button onClick={handleApi} >Login</button>
+    </div>
+  );
+}
+
+export default App;
